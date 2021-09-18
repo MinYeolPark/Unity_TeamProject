75db9b361ff30342dec98e566ede5a3ef002b27e
4arang <80613153+4arang@users.noreply.github.com>
2021-09-18 14:56:06 +0900

    0918_2
    확실히 안쓰는거만 정리

---
+++ TeamProject_0902/Assets/Effect/SkillshotEffect/ProjectilesFX/VFX_Resources/Scripts/PFX_ProjectileObject.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class PFX_ProjectileObject : MonoBehaviour
+{
+    public ParticleSystem FXToDeatch;
+    [HideInInspector]
+    public float Speed = 15f;
+    [HideInInspector]
+    public GameObject ImpactFX;
+    [HideInInspector]
+    public float ImpactFXDestroyDelay = 2f;
+    [HideInInspector]
+    public float ImpactOffset = 0.15f;
+
+    private Vector3 grenadeDir;
+
+   public void Setup(Vector3 ShootDir)
+    {
+       grenadeDir = ShootDir;
+    }
+
+    private void Start()
+    {
+       
+        Destroy(gameObject, 3.0f);
+    }
+
+    private void FixedUpdate()
+    {
+        if (Speed == 0)
+            return;
+        transform.position += grenadeDir * (Speed * Time.deltaTime);
+        //transform.localPosition += new Vector3(Direct.x * (Speed * Time.deltaTime),
+        //    Direct.y, Direct.z * (Speed * Time.deltaTime));
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //ignore collisions with projectile
+        var contact = collision.contacts[0];
+        if (contact.otherCollider.name.Contains("Projectile"))
+            return;
+
+        Speed = 0;
+
+        var hitPosition = contact.point + contact.normal * ImpactOffset;
+
+        if (ImpactFX != null)
+        {
+            var impact = Instantiate(ImpactFX, hitPosition, Quaternion.identity);
+            impact.transform.localScale = transform.localScale;
+            Destroy(impact, ImpactFXDestroyDelay);
+        }
+
+        FXToDeatch.transform.parent = null;
+        FXToDeatch.Stop(true);
+        Destroy(FXToDeatch.gameObject, ImpactFXDestroyDelay);
+
+        Destroy(gameObject);
+    }
+}
