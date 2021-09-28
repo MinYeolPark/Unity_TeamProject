fbc750fc0e3a5b773954b5a10a0b43ea8fb54080
박민열 <67354549+MinYeolPark@users.noreply.github.com>
2021-09-28 19:11:23 +0900

    09/28

---
+++ TeamProject_0902/Assets/Scripts/Network/ChampionDatabase.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChampionDatabase : MonoBehaviour
+{
+    public enum Champions
+    {
+        BaseChamp,
+        Xerion,
+        BaekRang,
+        ColD
+    }
+
+}
